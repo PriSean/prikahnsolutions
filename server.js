979d1a8aca8703c53b36f63ec6f64cb1ea95bf5a
Sean McCulloch <sean.mcculloch23@gmail.com>
2025-05-03 19:20:09 -0400

    fix: update server configuration and repository URL

--- server.js
+++ server.js
@@ -3,12 +3,22 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Enable compression
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Serve static files from the React app build folder
 app.use(express.static(path.join(__dirname, 'prikahn-solutions/build')));
 
 // Serve index.html on all unknown routes (for React Router)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'prikahn-solutions/build', 'index.html'));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 // Start the server
